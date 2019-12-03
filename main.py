77a0603e2d167e37b3bb7e2c9cff026b6bb65d9a
Tristan Jordan <35291790+CommandoTeeJ@users.noreply.github.com>
2019-12-03 15:52:33 -0500

    Changes to PCA

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from feature_selection import select_features
 dataset = pd.read_csv('data/student-mat.csv', delimiter=";")
 dataset = preprocessing.preprocess(dataset)
 x, y = preprocessing.split_attributes(dataset, 3)
-x = select_features(x, 5)
+x = select_features(x, 8)
 y = preprocessing.bucketize_y(y, 5)
 
 cols = list(x.columns)
@@ -15,7 +15,7 @@ cols.extend(y.columns)
 new_data = pd.DataFrame(data=x.join(y), columns=cols)
 
 #splitting train and test
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=.2,random_state=0)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=.15,random_state=0)
 
 #Neural Network
 import ann
