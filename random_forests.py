9124823ed84fbb6d9375562e8034961e641d280e
PoptartSmasher <37163137+PoptartSmasher@users.noreply.github.com>
2019-12-03 16:50:38 -0500

    Forests achieved

--- random_forests.py
+++ random_forests.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 def classify(x_train,x_test,y_train,y_test):
-    model = DecisionTreeClassifier(random_state=0)
+    model = RandomForestClassifier(n_estimators = 1000, criterion = 'entropy', random_state = 0)
     model.fit(x_train, y_train)
     test_pred = model.predict(x_test)
     confusionMat = confusion_matrix(y_test, test_pred)
