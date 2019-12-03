127de5759f541a0ecd2ac4429ae84121c0e08e31
Tristan Jordan <35291790+CommandoTeeJ@users.noreply.github.com>
2019-12-03 14:22:18 -0500

    Update ann.py

--- ann.py
+++ ann.py
@@ -1,4 +1,18 @@
-def build_and_train_net(data):
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+def build_and_train_net(data,classes):
+    
+    
+    classifier=Sequential()
+    classifier.add(Dense(activation="relu",input_dim=3))
+    classifier.add(Dense(activation="sigmoid"))
+    classifier.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
+    
+    classifier.fit(x)
+    classifier.save("Model.h5")
+
     pass
 
 def run_classifier(data):
